849d48e14690e84ab20c85e8666a56e694ed68be
webappsagency <agcastro.py@yahoo.com>
2019-09-22 20:46:30 -0300

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { GraphQLServer } = require('graphql-yoga')
+const Binding = require('prisma-binding')
+const { prisma } = require('./generated/prisma-client')
+
+const binding = new Binding.Prisma({
+  typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+  endpoint: process.env.PRISMA_ENDPOINT
+})
+
+const resolvers = {
+  Query: {
+    user(parent, args, context, info) {
+      // return prisma.user({ id: args.id})
+      return binding.query.user({ where: { id: args.id }}, info)
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: `${__dirname}/schema.graphql`,
+  resolvers
+})
+
+server.start().then(() => console.log('Server running on http://localhost:4000'))
