6afd48f3655fd0d64643af0c6ee8e514902cb60b
webappsagency <agcastro.py@yahoo.com>
2019-09-22 22:53:45 -0300

    added login resolver and  mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,11 +3,26 @@ const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = process.env.JWT_SECRET
 
+async function login(parent, {email, password}, ctx, info) {
+  const user = await ctx.db.query.user({where: {email}})
+  if (!user) {
+    throw new Error('Invalid credentials!')
+  }
+  const valid = await bcrypt.compare(password, user.password)
+  if (!valid) {
+    throw new Error('Invalid credentials!')
+  } 
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, {expiresIn: '2h'})
+  return {
+    token,
+    user
+  }
+}
+
 async function signup(parent, args, ctx, info) {
   const password = await bcrypt.hash(args.password, 10)
   const user = await ctx.db.mutation.createUser({data: { ...args, password }})
   const token = jwt.sign({ userId: user.id }, JWT_SECRET, {expiresIn: '2h'})
-  
   return {
     token,
     user
@@ -15,5 +30,6 @@ async function signup(parent, args, ctx, info) {
 }
 
 module.exports = {
+  login,
   signup
 }
